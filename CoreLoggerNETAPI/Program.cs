a4d958e87d52d7a438edbed644fe8a7cfbeca245
mddazure <mddazure@gmail.com>
2019-01-07 13:59:46 +0100

    add dwg

--- CoreLoggerNETAPI/Program.cs
+++ CoreLoggerNETAPI/Program.cs
@@ -14,26 +14,49 @@ using System.Threading;
 using Microsoft.Rest;
 
 namespace CoreLoggerNETAPI
-{
+{ 
     class Program
     {
+        
         static void Main(string[] args)
         {
             var azureServiceTokenProvider = new AzureServiceTokenProvider();
             var accessToken = azureServiceTokenProvider.GetAccessTokenAsync("https://management.core.windows.net/").Result;
 
+            //Usage usage = new Usage();
+
             string[] subscrArray = { "0245be41-c89b-4b46-a3cc-a705c90cd1e8" };
+            string[] regionArray = { "west-europe", "north-europe", "france-central","uk-west","east-us"};
 
             TokenCredentials tokcred = new TokenCredentials(accessToken);
 
             var canceltoken = new CancellationToken();
 
+            ComputeManagementClient compclient = new ComputeManagementClient(tokcred);
+
+            /*
             SubscriptionClient subclient = new SubscriptionClient(tokcred);
-            Console.WriteLine($"subscrId: {subclient.Subscriptions.Get(subscriptionId: subscrArray[0]).SubscriptionId}\ndisplayName {subclient.Subscriptions.Get(subscriptionId: subscrArray[0]).DisplayName}");
+            Console.WriteLine($"subscrId: {subclient.Subscriptions.Get(subscriptionId: subscrArray[0]).SubscriptionId}\ndisplayName: {subclient.Subscriptions.Get(subscriptionId: subscrArray[0]).DisplayName}\n\n");
+            */
+
+            compclient.SubscriptionId = subscrArray[0];
+            Console.WriteLine($"SubscrID: {compclient.SubscriptionId}\n");
+
+            foreach (var reg in regionArray)
+            {
+                Console.WriteLine($"Region: {reg}\n");
+
+                var usage = compclient.Usage.List("west-europe");
+
+                foreach (var us in usage)
+                {
+                    if (us.CurrentValue != 0)
+                    {
+                        Console.WriteLine($"Name: {us.Name.LocalizedValue}\nCurrentValue: {us.CurrentValue}\nLimit: {us.Limit}\n");
+                    }
+                }
+            }
             Console.ReadLine();
-
-
-
 
 
         }
